ee08c4acea599207b1d669b8c0dde0ad5803adfc
Jerry <runan.jerry.ye@gmail.com>
2022-09-10 11:35:00 -0700

    Rename to follow naming convention

--- Assets/Scripts/BackendApi/EventLogger.cs
+++ Assets/Scripts/BackendApi/EventLogger.cs
@@ -1,15 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 using System.Net.Http;
-using System.Text.Json;
 using System.Text;
-using System.Text.Json.Serialization;
 using Newtonsoft.Json;
-using System.Net;
+using UnityEngine;
 
-
-namespace DataAnalysis
+namespace BackendApi
 {
     public class DataForm
     {
@@ -17,23 +11,23 @@ namespace DataAnalysis
         public DataForm(int[] time) { timeOfLevels = time; }
     }
 
-    public class SendData : MonoBehaviour
+    public class EventLogger : MonoBehaviour
     {
-        private string server;
-        private static readonly HttpClient client = new HttpClient();
+        private readonly string _server;
+        private static readonly HttpClient Client = new HttpClient();
 
 
 
-        public SendData(string serverUrl)
+        public EventLogger(string serverUrl)
         {
-            server = serverUrl;
+            _server = serverUrl;
         }
         // Start is called before the first frame update
-        public async void send(int[] data)
+        public async void Log(int[] data)
         {
             var values = new DataForm(data);
             var stringContent = new StringContent(JsonConvert.SerializeObject(values), Encoding.UTF8, "application/json");
-            var response = await client.PostAsync(server, stringContent);
+            var response = await Client.PostAsync(_server, stringContent);
 
             if (System.Convert.ToInt32(response.StatusCode) == 200)
                 print("success!");
