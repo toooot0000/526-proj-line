d6f2c106b3178aec3021294adddc1f253d5cc958
Hui528 <huiw0528@gmail.com>
2022-09-17 11:14:41 -0700

    JSONReader update

--- Assets/Scripts/Utility/JSONReader.cs
+++ Assets/Scripts/Utility/JSONReader.cs
@@ -1,33 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class JSONReader : MonoBehaviour
+namespace Utility
 {
-    public TextAsset textJSON;
-    
-    [System.Serializable]
-    public class Player
+    public class JSONReader
     {
-        public string name;
-        public int health;
-    }
-
-    [System.Serializable]
-    public class PlayerList
-    {
-        public Player[] player;
-    }
-
-    public PlayerList myPlayerList = new PlayerList();
-
-    void Start()
-    {
-        myPlayerList = JsonUtility.FromJson<PlayerList>(textJSON.text);
-    }
-
-    void Update()
-    {
-        
+        public static T ImportJson<T>(string path)
+        {
+            TextAsset textAsset = Resources.Load<TextAsset>(path);
+            return JsonUtility.FromJson<T>(textAsset.text);
+        }
     }
 }
