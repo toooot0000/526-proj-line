421481a9ef0df112908f25c788f0664fb51afd24
Yuhuan Zhao <yuhuanzhao@Yuhuans-MacBook-Pro.local>
2022-09-24 16:37:24 -0700

    enemy

--- Assets/Scripts/Core/Model/Enemy.cs
+++ Assets/Scripts/Core/Model/Enemy.cs
@@ -9,6 +9,15 @@ namespace Core.Model{
         public int currentHp;
         public int attack = 1;
 
+        public int id;
+
+        public String desc;
+
+        public String special;
+
+        public int cooldown;
+
+
         public event ModelEvent OnAttack;
         public event ModelEvent OnBeingAttacked;
         public event ModelEvent OnDie;
@@ -18,9 +27,19 @@ namespace Core.Model{
         }
 
         public Enemy(GameModel parent, int id) : base(parent) {
+            this.id = id;
             var enemy = CsvLoader.TryToLoad("Configs/enemies", id);
             if (enemy == null) return;
-            
+            try {
+                type = EnumUtility.GetValue<Type>(ball["type"] as string);
+                desc = ball["desc"] as string;
+                special = ball["special"] as string;
+                cooldown = (int)ball["cooldown"];
+                hpUpLimit = (int)ball["hp"];
+                attack = (int)ball["attack"];
+            }
+            catch (Exception e) {
+            }
         }
         
         public void TakeDamage(Damage damage){
