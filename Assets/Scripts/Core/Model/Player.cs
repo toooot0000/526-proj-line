ddae83d7ca708150a9387f0d44439ba65414ae97
boweixu <bowei_xu@163.com>
2022-09-23 20:00:22 -0700

    revise comboeffect & chargeeffect

--- Assets/Scripts/Core/Model/Player.cs
+++ Assets/Scripts/Core/Model/Player.cs
@@ -13,7 +13,7 @@ namespace Core.Model{
         public int gearUpLimit;
         public int energy;
         public float armor;
-        
+
         public List<Ball> hitBalls = new();
         public List<Ball> circledBalls = new();
 
@@ -53,46 +53,96 @@ namespace Core.Model{
             OnAttack?.Invoke(currentGame, this);
         }
         
-        public Gear[] Add<Gear>(Gear[] gears, Gear item)
+        public Gear[] Add<Gear>(Gear gear)
         {
-            if (gears == null)
-            {
-                gears = new Gear[]{};
-            }
-
             Gear[] result = new Gear[gears.Length + 1];
             gears.CopyTo(result, 0);
-            result[gears.Length] = item;
+            result[gears.Length] = gear;
             return result;
         }
 
-        public Gear[] DeleteAt<Gear>(Gear[] gears, int index)
+        public Gear[] DeleteAt<Gear>(int index)
         {
-            Gear[] res = new Gear[gears.Length - 1];
+            Gear[] res = new Gear[gears.Length-1];
             if( index > 0 )
                 Array.Copy(gears, 0, res, 0, index);
 
-            if( index < gears.Length - 1 )
+            if( index < gears.Length-1 )
                 Array.Copy(gears, index + 1, res, index, gears.Length - index - 1);
 
             return res;
         }
 
-        public Gear GetItem<Gear>(Gear[] gears, int index, Gear defaultGear)
-        {
-            if (index < gears.Length)
-            {
-                return gears[index];
-            }
-            return defaultGear;
-        }
-
-        public Gear[] GetAllItem<Gear>(Gear[] gears)
+        public Gear[] GetAllGears()
         {
             Gear[] res = new Gear[gears.Length];
             gears.CopyTo(res, 0);
             return res;
         }
+
+        public float chargeEffect(List<Ball> circledBalls)
+        {
+            float points = 0;
+            if (circledBalls.Count == 0)
+            {
+                return points;
+            }
+            else
+            {
+                Dictionary<Ball, int> res = new Dictionary<Ball, int>();
+                for (int i = 0; i < circledBalls.Count; i++)
+                {
+                    if (res.ContainsKey(circledBalls[i]))
+                    {
+                        res[circledBalls[i]] += 1;
+                    }
+                    else
+                    {
+                        res.Add(circledBalls[i],1);
+                    }
+                }
+
+                foreach (Ball circleball in res.Keys)
+                {
+                    points += circleball.charge * circleball.point * res[circleball];
+                }
+            }
+            return points;
+        }
         
+        public float comboEffect(Player player,List<Ball> hitBalls, List<Ball> circledBalls){
+            float points = 0;
+            if(hitBalls.Count == 0) 
+            {
+                return points;
+            }
+            else
+            {
+                Dictionary<Ball, int> record = new Dictionary<Ball, int>();
+                for( int j = 0; j < hitBalls.Count; j++)
+                {
+                    if(record.ContainsKey(hitBalls[j])){
+                        record[hitBalls[j]] += 1;
+                    }else{
+                        record.Add(hitBalls[j], 1);
+                    }
+                }
+                foreach(Ball hitBall in record.Keys){
+                    if(record[hitBall] >= 2){
+                        if (hitBall.type != Ball.Type.Defend)
+                        {
+                            points += hitBall.combo * hitBall.point * record[hitBall];
+                        }
+                        else
+                        {
+                            points += hitBall.combo * hitBall.point * record[hitBall];
+                            player.armor += points;
+                        }
+                    }
+                }
+                return points;
+            }
+
+        }
     }
 }
