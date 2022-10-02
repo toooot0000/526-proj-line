d3cd88342d2601eb3cea00e03a476d48bc858cb3
Jerry Ye's Macbook Pro <runan.ye@outlook.com>
2022-10-02 15:09:40 -0700

    Adjust Z-ind and add bezier

--- Assets/Scripts/UI/GearDisplayer/GearDisplayer.cs
+++ Assets/Scripts/UI/GearDisplayer/GearDisplayer.cs
@@ -5,18 +5,8 @@ using UnityEngine;
 
 namespace UI.GearDisplayer{
     public class GearDisplayer: MonoBehaviour{
-        // public TextMeshProUGUI desc;
-        // public SpriteRenderer sprite;
         public UIContainerBase container;
         public GameObject gearItemPrefab;
-        
-        // public Gear Gear{
-        //     set{
-        //         desc.text = $"{value.name}\nAtt: {value.ball.point.ToString()}\nBall Num: {value.ballNum.ToString()}";
-        //         var textureSprite = Resources.Load<Sprite>(value.imgPath);
-        //         sprite.sprite = textureSprite;
-        //     }
-        // }
 
         private void Start(){
             GameManager.shared.game.player.OnGearChanged += UpdateGears;
