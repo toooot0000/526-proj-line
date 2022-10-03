0b0c9974257308992029918039900a3d0e6e14c2
Jerry Ye's Macbook Pro <runan.ye@outlook.com>
2022-10-02 17:54:13 -0700

    Extract Event

--- Assets/Scripts/BackendApi/EventLogger.cs
+++ Assets/Scripts/BackendApi/EventLogger.cs
@@ -8,30 +8,6 @@ using UnityEngine;
 using UnityEngine.InputSystem.Controls;
 
 namespace BackendApi{
-    public abstract class LoggableEvent {
-        public virtual string URLPath => throw new NotImplementedException();
-        public string uuid = GameManager.shared.uuid.ToString();
-    }
-
-    public class EventClearanceRecord: LoggableEvent{
-        public int level;
-        public string status; // "success" || "fail"
-        public int time;
-        public override string URLPath => "logClearanceRecord";
-    }
-    public class EventSkillUses: LoggableEvent{
-        public int skillId;
-        public int uses;
-        public override string URLPath => "logSkillUses";
-    }
-
-    public class EventItemInteract : LoggableEvent{
-        public int itemId;
-        public string status;
-        public int count;
-        public override string URLPath => "logItemsInteract";
-    }
-    
     /**
      * 
 //     using sample:
