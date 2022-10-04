edd4982d454e7758c311427c24c8547bd247f507
Jerry <runan.jerry.ye@gmail.com>
2022-10-03 23:50:55 -0700

    Adjust ball

--- Assets/Scripts/Core/PlayArea/Balls/BallConfig.cs
+++ Assets/Scripts/Core/PlayArea/Balls/BallConfig.cs
@@ -6,30 +6,20 @@ using Random = UnityEngine.Random;
 
 namespace Core.PlayArea.Balls{
     public class BallConfig: MonoBehaviour{
-
-        public static Dictionary<global::Model.BallType, Color> colors = new(){
-            {global::Model.BallType.Physics, Color.white},
-            {global::Model.BallType.Magic, Color.blue},
-            {global::Model.BallType.Defend, Color.yellow}
-        };
         public global::Model.Ball modelBall;
 
         public SpriteRenderer icon;
-        private Vector2 _originIconSize = new Vector2(1f, 1f);
 
         public SpriteRenderer sprite;
         public BallView ballView;
         
         
-        private void Start(){
-            _originIconSize = new Vector2(1f, 1f);
-        }
         
         public void UpdateConfig(){
             var rectTransform = transform as RectTransform;
             rectTransform!.localScale = new Vector3(modelBall.size, modelBall.size, 1);
             ballView.velocity = Vector2.one.Rotate(Random.Range(0, 360)).normalized * modelBall.speed;
-            sprite.color = colors[modelBall.type];
+            sprite.color = Color.white;
             icon.sprite = Resources.Load<Sprite>((modelBall.parent as Gear)!.imgPath);
             icon.color = Color.white;
         }
