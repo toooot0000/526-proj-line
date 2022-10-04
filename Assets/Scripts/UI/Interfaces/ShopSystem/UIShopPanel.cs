be6f8c6de522a6606d4638f09542e03d9a4ef67f
Hui528 <huiw0528@gmail.com>
2022-10-04 14:34:10 -0700

    update shop prefab

---
+++ Assets/Scripts/UI/Interfaces/ShopSystem/UIShopPanel.cs
@@ -0,0 +1,51 @@
+using Model;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI.Interfaces.ShopSystem {
+    public delegate void ClickEvent(UIShopPanel panel);
+
+    public class UIShopPanel : MonoBehaviour {
+        public TextMeshProUGUI text;
+        public Image image;
+        public Image highLight;
+        private CanvasGroup _group;
+
+        private Gear _model;
+        private Transform _parent;
+
+        public Gear Model {
+            set {
+                _model = value;
+                image.sprite = Resources.Load<Sprite>(value.imgPath);
+                text.text = value.desc;
+            }
+            get => _model;
+        }
+
+        public bool Show {
+            set {
+                gameObject.SetActive(value);
+                if (value) {
+                    transform.SetParent(_parent);
+                }
+                else {
+                    _parent = transform.parent;
+                    transform.SetParent(null);
+                }
+            }
+        }
+
+        private void Start() {
+            _group = GetComponent<CanvasGroup>();
+            _parent = transform.parent;
+        }
+
+        public event ClickEvent OnClick;
+
+        public void Click() {
+            OnClick?.Invoke(this);
+        }
+    }
+}
---
+++ Assets/Scripts/UI/Interfaces/ShopSystem/UIShopSystem.cs
@@ -0,0 +1,93 @@
+using BackendApi;
+using Model;
+using UI.Container;
+using UnityEngine;
+using Utility;
+
+namespace UI.Interfaces.ShopSystem {
+    public class UIShopSystem : UIBase {
+        public UIContainerFlexBox container;
+        public Gear[] items;
+        private CanvasGroup _canvasGroup;
+        private bool _inAnimation;
+        private UIShopPanel[] _panels;
+        // private UIShopPanel _selected;
+
+        private void Start() {
+            _canvasGroup = GetComponent<CanvasGroup>();
+            _canvasGroup.alpha = 0;
+            _panels = transform.GetComponentsInChildren<UIShopPanel>();
+        }
+
+        public override void Open(object gears) {
+            base.Open();
+            _inAnimation = true;
+            var coroutine = TweenUtility.Lerp(0.2f,
+                () => _canvasGroup.alpha = 0,
+                i => _canvasGroup.alpha = i,
+                () => _inAnimation = false
+            );
+            items = gears as Gear[];
+            LoadGearPanel();
+            StartCoroutine(coroutine());
+        }
+
+        public override void Close() {
+            _inAnimation = true;
+            var coroutine = TweenUtility.Lerp(0.2f,
+                () => _canvasGroup.alpha = 1,
+                i => _canvasGroup.alpha = 1 - i,
+                () => {
+                    _inAnimation = false;
+                    base.Close();
+                    Destroy(gameObject);
+                });
+            StartCoroutine(coroutine());
+        } // ReSharper disable Unity.PerformanceAnalysis
+        public void LoadGearPanel() {
+            var curPanelInd = 0;
+            if (items.Length > 6) {
+                Debug.LogError("Gears in Shop more than 6!");
+                return;
+            }
+
+            foreach (var gear in items) {
+                var gearPanel = _panels[curPanelInd];
+                gearPanel.OnClick += PurchaseSelectedGear;
+                gearPanel.Show = true;
+                gearPanel.Model = gear;
+                curPanelInd++;
+                if (gear.rarity > GameManager.shared.game.player.Coin) gearPanel.highLight.enabled = false;
+            }
+
+            for (; curPanelInd < _panels.Length; curPanelInd++) _panels[curPanelInd].Show = false;
+            container.UpdateLayout();
+        }
+
+        public void ConfirmButtonEvent() {
+            StartCoroutine(CoroutineUtility.Delayed(GameManager.shared.game.GoToNextStage));
+            // GameManager.shared.game.player.AddGear(_selected.Model);
+            // EventLogger.Shared.Log(new EventItemInteract {
+            //     itemId = _selected.Model.id,
+            //     status = "obtained",
+            //     count = 1
+            // });
+            Close();
+        }
+
+        // private void ChangeSelectedItemTo(UIShopPanel clickedPanel) {
+        //     if (_selected == clickedPanel) return;
+        //     if (_selected != null) _selected.highLight.enabled = false;
+        //     clickedPanel.highLight.enabled = true;
+        //     _selected = clickedPanel;
+        // }
+
+        private void PurchaseSelectedGear(UIShopPanel clickedPanel)
+        {
+            if (GameManager.shared.game.player.Coin < clickedPanel.Model.rarity) return;
+            GameManager.shared.game.player.Coin -= clickedPanel.Model.rarity;
+            GameManager.shared.game.player.AddGear(clickedPanel.Model);
+            LoadGearPanel();
+        }
+    }
+}
