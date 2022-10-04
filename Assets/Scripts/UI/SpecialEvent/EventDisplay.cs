001ba1d6b92dc88db429748ffb67fdb6a9175672
Lovy99 <362809422@qq.com>
2022-10-04 13:11:33 -0700

    event system
    basic interaction with model

--- Assets/Scripts/UI/SpecialEvent/EventDisplay.cs
+++ Assets/Scripts/UI/SpecialEvent/EventDisplay.cs
@@ -1,34 +1,18 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using Model;
 using Event = Model.Event;
 using TMPro;
-using UnityEngine.UIElements;
-using Image = UnityEngine.UI.Image;
-using Random = UnityEngine.Random;
 
 public class EventDisplay : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI questionText;
     [SerializeField] GameObject[] answerButtons;
-    [SerializeField]Sprite defaultAnswerSprite;
-    [SerializeField]Sprite selectedAnswerSprite;
-    [SerializeField] int eventIndex = 4;
-
-    private void Awake()
-    {
-        //eventIndex = Random.Range(0, 3);
-        //eventIndex = 4;
-    }
-
     // Start is called before the first frame update
     void Start()
     {
-        
-        Event currentEvent = new Event(eventIndex);
+        Event currentEvent = new Event(0);
         questionText.text = currentEvent.getQuestion();
         
         for (int i = 0; i < answerButtons.Length; i++)
@@ -37,22 +21,14 @@ public class EventDisplay : MonoBehaviour
             buttonText.text = currentEvent.getAnswer(i);
         }
         
-        Debug.Log(GameManager.shared.game.player.gears.Count);
-
     }
 
     public void OnAnswerSelected(int index)
     {
-        Debug.Log("pressed");
-        Event e = new Event(eventIndex);
-        questionText.text = index.ToString();
-        e.explainArgs(index);
-        Image buttonImage = answerButtons[index].GetComponent<Image>();
-        buttonImage.sprite = selectedAnswerSprite;
-        Debug.Log(GameManager.shared.game.player.gears.Count);
-        
+        //Event e = new Event(0);
+        questionText.text = "pressed";
+        //e.explainArgs(index);
     }
-    
     
 }
 
