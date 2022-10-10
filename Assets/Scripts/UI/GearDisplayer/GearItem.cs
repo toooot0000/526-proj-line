495337b621c41b7e1ea9be11e1f0cc213c288868
boweixu <bowei_xu@163.com>
2022-10-10 16:50:05 -0700

    adjust the display

--- Assets/Scripts/UI/GearDisplayer/GearItem.cs
+++ Assets/Scripts/UI/GearDisplayer/GearItem.cs
@@ -7,6 +7,8 @@ namespace UI.GearDisplayer{
     public class GearItem : MonoBehaviour{
         public Image image;
         public TextMeshProUGUI textMesh;
+        public TextMeshProUGUI ballspeed;
+        public TextMeshProUGUI ballNum;
 
         private Gear _model;
 
@@ -21,6 +23,25 @@ namespace UI.GearDisplayer{
         public void UpdateContent(){
             image.sprite = Resources.Load<Sprite>(_model.imgPath);
             textMesh.text = Model.ToDescString();
+            
+            if (_model.ball.speed <= 0)
+            {
+                ballspeed.text = "";
+            }
+            if (_model.ball.speed > 0 && _model.ball.speed <= 0.5)
+            {
+                ballspeed.text = ">";
+            }
+            if ( _model.ball.speed > 0.5 && _model.ball.speed <= 1.5)
+            {
+                ballspeed.text = ">>";
+            }
+            else
+            {
+                ballspeed.text = ">>>";
+            }
+            
+            ballNum.text = $"x {_model.ballNum.ToString()}";
         }
     }
 }
