02944163b5da2b0d8eabf852748764c9c1830f51
Jerry <runan.jerry.ye@gmail.com>
2022-10-12 23:22:41 -0700

    Add RemainingEnemy.cs

---
+++ Assets/Scripts/UI/NextEnemyDisplayer/NextEnemyDisplayer.cs
@@ -0,0 +1,19 @@
+using UI.Common.SimpleAnimation;
+
+namespace UI.NextEnemyDisplayer{
+    public class NextEnemyDisplayer: UIComponent{
+        public EdgeHider edgeHider;
+
+        private void Start(){
+            UIManager.shared.RegisterComponent(this);
+        }
+
+        public override void Hide(){
+            edgeHider.Hide();
+        }
+
+        public override void Show(){
+            edgeHider.Show();
+        }
+    }
+}
