e22538dc4a06722d954ef4d03464df9c331843c1
Jerry <runan.jerry.ye@gmail.com>
2022-10-18 04:14:22 -0700

    UIAdjustment

--- Assets/Scripts/Tutorial/Utility/StepTapToContinue.cs
+++ Assets/Scripts/Tutorial/Utility/StepTapToContinue.cs
@@ -1,9 +1,6 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using Tutorial.Common;
-using Unity.VisualScripting;
-using Unity.VisualScripting.Dependencies.Sqlite;
 using UnityEngine;
 
 namespace Tutorial.Utility{
