640baf6ba213db27a4aa7ce5c5875aa641c55e60
Jerry <runan.jerry.ye@gmail.com>
2022-10-17 17:19:11 -0700

    Make Envent inherent GameModel

--- Assets/Scripts/Model/Event.cs
+++ Assets/Scripts/Model/Event.cs
@@ -6,12 +6,12 @@ using Utility.Loader;
 
 namespace Model{
 
-    public class EventChoice{
+    public class EventChoice: GameModel{
         public string desc;
         public string[] effectToken;
         public int[] values;
 
-        public EventChoice(string fromString){
+        public EventChoice(GameModel parent, string fromString): base(parent){
             var result = EventChoice.ParseArgs(fromString);
             desc = result[0] as string;
             effectToken = result[1] as string[];
@@ -38,7 +38,7 @@ namespace Model{
         }
     }
     
-    public class Event{
+    public class Event: GameModel{
         public readonly string[] argsArray = new string[3];
         public readonly string desc;
         public int id;
@@ -47,7 +47,7 @@ namespace Model{
         public readonly EventChoice[] choices;
 
 
-        public Event(int id){
+        public Event(GameModel parent, int id): base(parent){
             var event1 = CsvLoader.TryToLoad("Configs/events", id);
             if (event1 == null){
                 Debug.LogError("Event not found");
@@ -62,9 +62,9 @@ namespace Model{
             argsArray[1] = event1["arg2"] as string;
             argsArray[2] = event1["arg3"] as string;
             choices = new[]{
-                new EventChoice(argsArray[0]),
-                new EventChoice(argsArray[1]),
-                new EventChoice(argsArray[2])
+                new EventChoice(this, argsArray[0]),
+                new EventChoice(this, argsArray[1]),
+                new EventChoice(this, argsArray[2])
             };
         }
 
