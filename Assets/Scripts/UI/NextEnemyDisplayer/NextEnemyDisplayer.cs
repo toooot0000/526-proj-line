5ef07a9ea9dd9b83647341a9d12ec1c4c32a19f0
Lovy99 <362809422@qq.com>
2022-10-18 00:24:27 -0700

    enemyItem class complete

--- Assets/Scripts/UI/NextEnemyDisplayer/NextEnemyDisplayer.cs
+++ Assets/Scripts/UI/NextEnemyDisplayer/NextEnemyDisplayer.cs
@@ -9,27 +9,20 @@ namespace UI.NextEnemyDisplayer{
         public EdgeHider edgeHider;
         public Image enemyImage;
         public TextMeshProUGUI nameText;
-        public Image _img;
+        public EnemyItem enemyItemPrefab;
+        private EnemyItem shownEnemyItem;
 
         private void Start(){
             GameManager.shared.game.currentStage.OnEnemyChanged += UpdateEnemy;
             UIManager.shared.RegisterComponent(this);
-            UpdateEnemy(GameManager.shared.game, GameManager.shared.game.currentStage);
+            //UpdateEnemy(GameManager.shared.game, GameManager.shared.game.currentStage);
         }
 
         private void UpdateEnemy(Game game, GameModel stage)
         {
-            Enemy nextEnemy = (stage as Stage)!.NextEnemy;
-            if(nextEnemy == null)
-            {
-                //GameObject.GetComponent<Image>().enabled = false;
-                GameObject.Find("Image").SetActive(false);
-                nameText.text = "No More";
-                enemyImage.sprite = null;
-                return;
-            }
-            nameText.text = nextEnemy.desc;
-            enemyImage.sprite = Resources.Load<Sprite>(nextEnemy.imgPath);
+            if(shownEnemyItem != null) Destroy(shownEnemyItem.gameObject);
+            shownEnemyItem = Instantiate(enemyItemPrefab, transform);
+            shownEnemyItem.UpdateContent();
         }
         
 
