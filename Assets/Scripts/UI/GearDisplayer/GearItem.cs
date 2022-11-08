bf9db2110701935f408b1698029822f61cf9f965
boweixu <bowei_xu@163.com>
2022-11-08 15:51:17 -0800

    add gearinfo in GearDisplayer

--- Assets/Scripts/UI/GearDisplayer/GearItem.cs
+++ Assets/Scripts/UI/GearDisplayer/GearItem.cs
@@ -8,18 +8,11 @@ using Utility.Loader;
 
 namespace UI.GearDisplayer{
     public class GearItem : MonoBehaviour{
-        
-        public TextMeshProUGUI textMesh;
-        public TextMeshProUGUI ballspeed;
-        public TextMeshProUGUI ballNum;
-        public Image image;
-        
+
         public Image icon;
+        private Gear _model;
         public float ballsize;
         
-        private Gear _model;
-        private Rect _rectTransRect;
-
         public Gear Model{
             set{
                 _model = value;
@@ -28,20 +21,17 @@ namespace UI.GearDisplayer{
             get => _model;
         }
 
+        public void ShowGearInfo()
+        {
+            UIManager.shared.OpenUI("UIGearInfo", _model);
+        }
+        
         public void UpdateContent()
         {
             var ball = CsvLoader.TryToLoad("Configs/balls", Model.ballId);
             ballsize = Mathf.Min((float)ball["size"], 1.5f);
             (transform as RectTransform)!.localScale = new Vector3(ballsize, ballsize, 1);
             icon.sprite = Resources.Load<Sprite>(Model.imgPath);
-            textMesh.text = Model.ToDescString();
-            ballspeed.text = (float)ball["speed"] switch {
-                <=0 => "",
-                >0 and <=0.5f => ">",
-                >0.5f and <=1.5f => ">>",
-                _ => ">>>"
-            };
-            ballNum.text = $"x {Model.ballNum.ToString()}";
         }
     }
 }
